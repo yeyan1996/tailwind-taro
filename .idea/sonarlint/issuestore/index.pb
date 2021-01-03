499077cec7e166ba63382aa70233345a34bb5de7
zhouhaolei <zhouhaolei@bytedance.com>
2021-01-03 10:19:39 +0800

    chore: init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,22 @@
+
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+C
+project.config.json,0/0/000e81eecb3dc940708c485fd21934b53a135fcd
+I
+src/pages/index/index.vue,a/4/a45bbdb3e25daad44d2e67981e0b7025c481cc61
+9
+	yarn.lock,5/3/53585a096fe3a3b96321113856ae62602995a33b
+@
+src/tailwind.css,d/1/d1ef6ccb260115c9f3a48b68535c9db7487e4ce6
+B
+tailwind.config.js,0/9/0918cbb76f2c44f223706cec9d31a32cd1177ce4
+6
+.npmrc,1/2/12453d06e6e35287172e482940b954ba12dc25a2
+?
+config/index.js,a/a/aa8a70fc9f39394872921561977e736d2ac97a3c
+<
+src/app.less,9/a/9a52500949cceb8939e04c564c843c895418e46b
+:
+
+src/app.ts,2/1/216381173f187cf4c2baf119193855699f4bc616
---
+++ src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="mb-20 white-children">
+    <view class="w-1_4 bg-blue-400 h-40">25% width</view>
+    <view class="w-1_2 bg-blue-400 h-40">50% width</view>
+    <view class="w-3_4 bg-blue-400 h-40">75% width</view>
+    <view class="w-full bg-blue-400 h-40">100% width</view>
+  </view>
+
+  <view class="bg-purple-300 mb-20">
+    <text class="font-mono">
+      font-mono: The quick brown fox jumps over the lazy dog.
+    </text>
+  </view>
+
+  <view class="flex items-start bg-pink-300 mb-20">
+    <view class="mr-20">align-start</view>
+    <view class="h-120 w-80 bg-purple-600 rounded text-white">1</view>
+    <view class="h-240 w-80 ml-10 bg-purple-600 rounded text-white">2</view>
+    <view class="h-160 w-80 ml-10 bg-purple-600 rounded text-white">3</view>
+  </view>
+
+  <view class="border-solid border-4 border-blue-500 rounded mb-20 flex justify-center">
+      border
+  </view>
+
+  <view class="rounded bg-yellow-500 w-100 mb-20 ml-1_3 flex justify-center text-white">rounded</view>
+
+  <view class="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-30 flex justify-center items-center">
+    <text class="text-white">bg-gradient-to-r</text>
+  </view>
+
+  <button class="text-sm w-200 py-3 rounded-md font-semi bold text-white bg-blue-500 shadow-2xl mt-20">shadow</button>
+</template>
+
+
+<style>
+.white-children view{
+  @apply text-white
+}
+</style>
